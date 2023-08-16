a1839e8d3002459dc633432740a1c46e4be3affc
edwyncoder <edwyn.t@flyerssoft.com>
2023-08-16 23:22:47 +0530

    added validation middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 
 const UserController = require('../../controllers/user-controller');
+const {AuthRequestValidators} = require('../../middlewares/index');
 
 const router = express.Router();
 
 router.post(
     '/signup', 
+    AuthRequestValidators.validateUserAuth,
     UserController.create
 );
 
 router.post(
     '/signin',
+    AuthRequestValidators.validateUserAuth,
     UserController.signIn
 );
 
