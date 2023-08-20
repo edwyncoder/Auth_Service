ef4e305243547fcbe522cdcd15e4d770d5f1c6f2
edwyncoder <edwyn.t@flyerssoft.com>
2023-08-20 13:13:42 +0530

    added isAdmin api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+// const {User,Role} = require('./models/index')
 
 const app = express();
 
@@ -15,6 +17,14 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+
+        // let u1 = await User.findByPk(1)
+        // console.log(u1);
+        // let r1 = await Role.findByPk(1)
+        // await u1.addRole(r1)
     });
 }   
 
